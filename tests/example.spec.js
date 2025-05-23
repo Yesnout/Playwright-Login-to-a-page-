dc03de228a731c8e958bdabbae8aadda2d63cf4b
Yesnout <encarnacioncastroyesselernesto@gmail.com>
2025-05-22 23:59:55 -0400

    example.spec.js
    Need to has this name
    and before you put the code, create a file and put this command on the terminal "npm init playwright@latest"
    
    for test it, has two options, extension for vscode or type on the terminal "npx playwright test --ui"

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,31 +1,36 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('Login en Self Service desde el portal de Banner', async ({ page, context}) => {
+test('Login', async ({ page, context}) => {
   //  Go to banner portal
-  await page.goto('https://landing.unapec.edu.do/banner/'); //There you can adjust the URL in base of the website that you're in
+  await page.goto('the first link for the log in'); //There you can adjust the URL in base of the website that you're in
 
   
 
   // wait to the page charge the informationn
-  //Below this text says "Nuevapage" (you can name however you prefer) 
+  //Below this text says "Nuevapage" (you can name however you prefer) that's because whem you click specifically the button before, 
+  // it open a new link, sometimes is on the same, sometimes is no 
   const [Nuevapage] = await Promise.all([
-    context.waitForEvent('page'), // Esperar a que se abra una nueva página
-    page.click('a[href="https://alumnos.unapec.edu.do/StudentSelfService"] '), // Hacer clic en el botón
+    context.waitForEvent('page'), //this command is to wait for the page 1 to chargen so i can load
+    page.click('the link for the newpage"] '), // This is for the system click the button
   ]);
 
-  // 4. Llenar el correo institucional
-  await Nuevapage.locator('#i0116').fill('y.encarnacion14@unapec.edu.do'); // Esperar a que el campo de correo esté disponible
-  //await page.fill('input[type="email"]', '');
+  // 4. There's fill the institutional email
+  await Nuevapage.locator('There goes the email label id with a "#" at the beginning ').fill('There goes the email'); // wait for the email field to be available
 
-  //click en el boton de siguiente
-  await Nuevapage.locator('#idSIButton9').click();
 
-  // 5. Llenar la contraseña
-  await Nuevapage.locator('#i0118').fill('Codigo.2006'); // Esperar a que el campo de contraseña esté disponible
+  //click on the nex button(in this case is because it redirect to the next parte(password thing))
+  await Nuevapage.locator('The id for the button').click();
+
+  // 5. fill the password
+  await Nuevapage.locator('id for the paswword label').fill('password'); // wait for the password field to be available
   
-  //El click de la contraseña
-  await Nuevapage.locator("#idSIButton9").click();
-//Click para el mantener inicio de sesion
-  await Nuevapage.locator("#idSIButton9").click();
+  //password button
+  await Nuevapage.locator("#button id").click(); //this is the button for the password
+//This is for the question "Stay signed in?"
+  await Nuevapage.locator("#idSIButton9").click(); //this is for case that request you stay sign in after
 });
+
+
+//And that's all :)
+//remember, yo need to has a the playwright extension on vscode and also on the command promt as "npm i playwright"
